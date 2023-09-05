53bcddbb6b554d2e644f61c128f73779fa5f6d67
alexmainer <alexmainer31@gmail.com>
2023-09-05 23:29:58 +0300

    initial commit

---
+++ app/src/main/java/com/example/firebase_auth/data/AuthViewModel.kt
@@ -0,0 +1,69 @@
+package com.example.firebase_auth.data
+
+import android.content.Context
+import android.widget.Toast
+import androidx.navigation.NavHostController
+import com.example.firebase_auth.navigation.ROUTE_LOGIN
+import com.example.firebase_auth.navigation.ROUTE_REGISTER
+import com.google.firebase.auth.FirebaseAuth
+
+class AuthViewModel(var controller:NavHostController,var context:Context) {
+
+    var mAuth:FirebaseAuth
+
+    init {
+        mAuth=FirebaseAuth.getInstance()
+    }
+    fun signup(email:String,pass:String,confpass:String){
+
+
+        if (email.isBlank() || pass.isBlank() || confpass.isBlank()){
+            Toast.makeText(context,"Please fill the blanks",Toast.LENGTH_LONG).show()
+            return
+
+        }
+        else if (pass != confpass){
+            Toast.makeText(context,"Password do not match",Toast.LENGTH_LONG).show()
+            return
+        }
+        else{
+            mAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener {
+                if (it.isSuccessful){
+                    Toast.makeText(context,"Account Created Successfully",Toast.LENGTH_LONG).show()
+                }
+                else{
+                    Toast.makeText(context,"${it.exception!!.message}",Toast.LENGTH_LONG).show()
+                }
+
+            }
+        }
+
+
+    }
+
+    fun login(email: String,pass: String){
+        mAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener {
+            if (it.isSuccessful){
+                Toast.makeText(context,"LogIn successful",Toast.LENGTH_LONG).show()
+//                controller.navigate(ROUTE_REGISTER)...TO TAKE YOU TO A DIFF PAGE
+            }
+            else{
+                Toast.makeText(context,"${it.exception!!.message}",Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    fun logout(){
+        mAuth.signOut()
+        controller.navigate(ROUTE_LOGIN)
+    }
+    fun loggedin():Boolean{
+        return mAuth.currentUser !=null
+    }
+
+
+
+
+
+
+}
